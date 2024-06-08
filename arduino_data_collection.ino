79b087583bc9aa75acc1047865be0cd122bd0713
gain907 <58543428+gain907@users.noreply.github.com>
2024-06-08 18:55:39 +0900

    Update arduino_data_collection.ino

--- arduino_data_collection.ino
+++ arduino_data_collection.ino
@@ -1,6 +1,6 @@
 #include <Servo.h>
 #include <AFMotor.h>
-#include <SoftwareSerial.h> 
+#include <SoftwareSerial.h>
 
 // 모터 정의
 AF_DCMotor motor1(1);  // 모터 쉴드 M1 연결
@@ -12,15 +12,12 @@ AF_DCMotor motor4(4);  // 모터 쉴드 M4 연결
 Servo servo1;
 Servo servo2;
 
-int servo1Pos, servo2Pos;
-int speedDelay = 20;
+int servo1Pos = 90;
+int servo2Pos = 90;
+const int speedDelay = 20;
 
 // 모터 속도 및 방향 상태 변수
-int motor1Speed = 80;
-int motor2Speed = 80;
-int motor3Speed = 80;
-int motor4Speed = 80;
-
+int motorSpeed = 80;
 char motor1Direction = 'S'; // S: Stop, F: Forward, B: Backward
 char motor2Direction = 'S';
 char motor3Direction = 'S';
@@ -34,19 +31,13 @@ void setup() {
 
   servo1.attach(9);   // 서보 모터 1 핀
   servo2.attach(10);   // 서보 모터 2 핀
-  servo1Pos = 90;
-  servo2Pos = 90;  
-
   servo1.write(servo1Pos);
   servo2.write(servo2Pos);
-  
+
   pinMode(relayPin, OUTPUT);  // 릴레이 출력 설정
 
   // 모터 속도 설정
-  motor1.setSpeed(motor1Speed);
-  motor2.setSpeed(motor2Speed);
-  motor3.setSpeed(motor3Speed);
-  motor4.setSpeed(motor4Speed);
+  setMotorSpeed(motorSpeed);
 }
 
 void loop() {
@@ -54,71 +45,39 @@ void loop() {
     char command = Serial.read();
     Serial.print("Command:");
     Serial.println(command);
-    switch (command) {
-      case 'F':
-        moveForward();
-        break;
-      case 'B':
-        moveBackward();
-        break;
-      case 'L':
-        moveSidewaysLeft();
-        break;
-      case 'R':
-        moveSidewaysRight();
-        break;
-      case 'Q':
-        moveLeftForward();
-        break;
-      case 'E':
-        moveRightForward();
-        break;
-      case 'Z':
-        moveLeftBackward();
-        break;
-      case 'C':
-        moveRightBackward();
-        break;
-      case 'T':
-        rotateLeft();
-        break;
-      case 'Y':
-        rotateRight();
-        break;
-      case 'S':
-        stop();
-        break;
-      case 'U':
-        moveServo1up();
-        break;
-      case 'D':
-        moveServo1down();
-        break;
-      case 'I':
-        moveServo2up();
-        break;
-      case 'K':
-        moveServo2down();
-        break;
-      case 'A':
-        activateRelay();
-        break;
-      case 'V':
-        sleepRelay();
-        break;
-    }
+    executeCommand(command);
 
     // 모터와 서보 모터 상태 전송
-    Serial.print("M1:"); Serial.print(motor1Speed);
-    Serial.print(",D1:"); Serial.print(motor1Direction);
-    Serial.print(",M2:"); Serial.print(motor2Speed);
-    Serial.print(",D2:"); Serial.print(motor2Direction);
-    Serial.print(",M3:"); Serial.print(motor3Speed);
-    Serial.print(",D3:"); Serial.print(motor3Direction);
-    Serial.print(",M4:"); Serial.print(motor4Speed);
-    Serial.print(",D4:"); Serial.print(motor4Direction);
-    Serial.print(",S1:"); Serial.print(servo1Pos);
-    Serial.print(",S2:"); Serial.println(servo2Pos);
+    sendStatus();
+  }
+}
+
+void setMotorSpeed(int speed) {
+  motor1.setSpeed(speed);
+  motor2.setSpeed(speed);
+  motor3.setSpeed(speed);
+  motor4.setSpeed(speed);
+}
+
+void executeCommand(char command) {
+  switch (command) {
+    case 'F': moveForward(); break;
+    case 'B': moveBackward(); break;
+    case 'L': moveSidewaysLeft(); break;
+    case 'R': moveSidewaysRight(); break;
+    case 'Q': moveLeftForward(); break;
+    case 'E': moveRightForward(); break;
+    case 'Z': moveLeftBackward(); break;
+    case 'C': moveRightBackward(); break;
+    case 'T': rotateLeft(); break;
+    case 'Y': rotateRight(); break;
+    case 'S': stop(); break;
+    case 'U': moveServo1up(); break;
+    case 'D': moveServo1down(); break;
+    case 'I': moveServo2up(); break;
+    case 'K': moveServo2down(); break;
+    case 'A': activateRelay(); break;
+    case 'V': sleepRelay(); break;
   }
 }
 
@@ -127,10 +86,7 @@ void moveForward() {
   motor2.run(FORWARD);
   motor3.run(FORWARD);
   motor4.run(FORWARD);
-  motor1Direction = 'F';
-  motor2Direction = 'F';
-  motor3Direction = 'F';
-  motor4Direction = 'F';
+  updateDirections('F');
 }
 
 void moveBackward() {
@@ -138,10 +94,7 @@ void moveBackward() {
   motor2.run(BACKWARD);
   motor3.run(BACKWARD);
   motor4.run(BACKWARD);
-  motor1Direction = 'B';
-  motor2Direction = 'B';
-  motor3Direction = 'B';
-  motor4Direction = 'B';
+  updateDirections('B');
 }
 
 void moveSidewaysLeft() {
@@ -237,10 +190,14 @@ void stop() {
   motor2.run(RELEASE);
   motor3.run(RELEASE);
   motor4.run(RELEASE);
-  motor1Direction = 'S';
-  motor2Direction = 'S';
-  motor3Direction = 'S';
-  motor4Direction = 'S';
+  updateDirections('S');
+}
+
+void updateDirections(char direction) {
+  motor1Direction = direction;
+  motor2Direction = direction;
+  motor3Direction = direction;
+  motor4Direction = direction;
 }
 
 void moveServo1up() {
@@ -273,4 +230,17 @@ void activateRelay() {
 
 void sleepRelay() {
   digitalWrite(relayPin, LOW);
+}
+
+void sendStatus() {
+  Serial.print("M1:"); Serial.print(motor1Speed);
+  Serial.print(",D1:"); Serial.print(motor1Direction);
+  Serial.print(",M2:"); Serial.print(motor2Speed);
+  Serial.print(",D2:"); Serial.print(motor2Direction);
+  Serial.print(",M3:"); Serial.print(motor3Speed);
+  Serial.print(",D3:"); Serial.print(motor3Direction);
+  Serial.print(",M4:"); Serial.print(motor4Speed);
+  Serial.print(",D4:"); Serial.print(motor4Direction);
+  Serial.print(",S1:"); Serial.print(servo1Pos);
+  Serial.print(",S2:"); Serial.println(servo2Pos);
 }
